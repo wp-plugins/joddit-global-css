f9c906f63ba0f1b8e80299879bb7667a87a543fe
joddit <joddit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-22 09:30:21 +0000

    Fixed non-root redirection bug... Tagging version 0.7.5
    git-svn-id: https://plugins.svn.wordpress.org/joddit-global-css/tags/0.7.5@628582 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/pages/dashboard.php
+++ admin/pages/dashboard.php
@@ -9,8 +9,8 @@ $options = get_option( 'jgcss' );
 
 $jgcss_admin_pages->admin_header(
 	'Dashboard',
-	array('label' => 'Add New', 'url' => '/wp-admin/admin.php?page=jgcss_stylesheet'),
-	array('name' => 'bulkaction', 'action' => '/wp-admin/admin.php?page=jgcss_dashboard&amp;bulkaction=1', 'method' => 'post', 'id' => 'bulkaction')
+	array('label' => 'Add New', 'url' => site_url() . '/wp-admin/admin.php?page=jgcss_stylesheet'),
+	array('name' => 'bulkaction', 'action' => site_url() . '/wp-admin/admin.php?page=jgcss_dashboard&amp;bulkaction=1', 'method' => 'post', 'id' => 'bulkaction')
 );
 
 echo '<h2>' . 'Stylesheets' . '</h2>';
